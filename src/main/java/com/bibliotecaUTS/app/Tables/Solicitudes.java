469829dade9a0049b56c40311fc84d84c56516bf
Adrian <Adrian@LAPTOP-67S74O8K>
2024-05-29 15:33:13 -0500

    Nube

---
+++ src/main/java/com/bibliotecaUTS/app/Tables/Solicitudes.java
@@ -0,0 +1,60 @@
+package com.bibliotecaUTS.app.Tables;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name="solicitudes")
+public class Solicitudes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "libro_id", nullable = false)
+    private Libro libro;
+
+    @NotNull
+    @Column(name = "fecha_regreso")
+    private LocalDate fechaRegreso;
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public LocalDate getFechaRegreso() {
+        return fechaRegreso;
+    }
+
+    public void setFechaRegreso(LocalDate fechaRegreso) {
+        this.fechaRegreso = fechaRegreso;
+    }
+}
